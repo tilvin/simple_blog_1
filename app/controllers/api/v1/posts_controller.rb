7289bc78f1b7a9ffcbe1ff73cd6892ee8562c49e
Tilvin <kate2homy@gmail.com>
2019-06-05 14:03:26 +0300

    return all posts for logged user

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,12 +3,12 @@ class Api::V1::PostsController < Api::V1::BaseController
   before_action :identify_user
 
   def index
-    posts = @user.posts.with_attached_cover_image
+    posts = Post.with_attached_cover_image
     render json: PostSerializer.new(posts).serialized_json
   end
 
   def show
-    post = @user.posts.find(params[:id])
+    post = Post.find(params[:id])
     render json: PostSerializer.new(post).serialized_json
   end
 
